0a7ebc69630e37e309e54a63f81102ac5ff16431
Hassan Javaid Rao <hassanjrao@gmail.com>
2021-08-28 13:35:49 +0500

    updated db

--- delete_product.php
+++ delete_product.php
@@ -13,34 +13,60 @@ if (empty($_COOKIE['remember_me'])) {
     }
 }
 
-if($_SESSION['role']!=2){
+if ($_SESSION['role'] != 2) {
     header('location:index.php');
 }
 
-$request=NULL;
+$request = NULL;
 
 if (isset($_GET['id'])) {
 
     $del_id = $_GET["id"];
 
     try {
-       
+
+        $query = $conn->prepare(
+            "SELECT * from order_items WHERE product_id='$del_id' "
+        );
+        $query->execute();
+
+        $order_arr = array();
+        while ($result = $query->fetch(PDO::FETCH_ASSOC)) {
+
+
+
+            $order_id = $result["order_id"];
+
+            array_push($order_arr,$order_id);
+
+            $del = $conn->prepare("DELETE FROM order_items WHERE order_id='$order_id'");
+            $del->execute();
+
+            $del = $conn->prepare("DELETE FROM orders WHERE id='$order_id'");
+            $del->execute();
+        }
+
+        // foreach ($order_arr as $key => $id) {
+        //     # code...
+
+        //     $del = $conn->prepare("DELETE FROM orders WHERE id='$id'");
+        //     $del->execute();
+        // }
+
+
+
         $del = $conn->prepare("DELETE FROM products WHERE id='$del_id'");
         $del->execute();
 
-        $request="success";
+        $request = "success";
 
         header("location: all_products.php?request=$request");
-
-      } catch(PDOException $e) {
-        $request= "<br>" . $e->getMessage();
+    } catch (PDOException $e) {
+        $request = "<br>" . $e->getMessage();
         header("location: all_products.php?request=$request");
-      }
-
-
-}
-else{
-    header("location: all_products.php");   
+    }
+} else {
+    header("location: all_products.php");
 }
 
 
